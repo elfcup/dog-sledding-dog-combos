fa5caf46077877381be8bc0841670de9537f3c7d
danicoles <colescodes@gmail.com>
2018-04-21 14:21:12 -0700

    logic works, some images added

--- client/src/breeds.js
+++ client/src/breeds.js
@@ -1,8 +1,49 @@
 const breeds = [
     {
+        name: "Huksy",
+        image: "./img/husky.jpg",
+        score: [
+            4,
+            4,
+            4,
+            4,
+            4
+        ]
+    },
+    {
+        name: "Labrador",
+        image: "./img/lab.jpg",
+        score: [1, 2, 3, 4, 5]
+    },
+    {
+        name: "Maltese",
+        image: "./img/maltese.jpg",
+        score: [
+            2,
+            2,
+            2,
+            2,
+            2
+        ]
+    },
+    {
+        name: "Chihuahua",
+        image: "./img/chi.jpg",
+        score: [
+            1,
+            2,
+            1,
+            2,
+            1
+        ]
+    }
+    {
         name: "Poodle",
         image: "./img/poodle.jpg",
         score: [
+            3,
+            3,
+            3,
             3,
             3
         ]
@@ -10,12 +51,19 @@ const breeds = [
         name: "Lhasa Apso",
         image: "./img/poodle.jpg",
         score: [
-            1, 1
+            1, 
+            1,
+            1,
+            1,
+            1
         ]
     }, {
         name: "Vizsla",
         image: "./img/lab.jpg",
         score: [
+            5,
+            5,
+            5,
             5,
             5
         ]
